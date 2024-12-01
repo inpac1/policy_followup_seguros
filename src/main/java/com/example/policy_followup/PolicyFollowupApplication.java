2b5829e0a6f2c0d40656cc58efed74f0d3fc2d37
Ian <ian@miseguroaqui.com>
2024-12-01 14:02:44 -0600

    Initial commit

---
+++ src/main/java/com/example/policy_followup/PolicyFollowupApplication.java
@@ -0,0 +1,13 @@
+package com.example.policy_followup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyFollowupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolicyFollowupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/policy_followup/PolicyFollowupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.policy_followup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyFollowupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
