cdeae22992c6771a6e425de2018723f2f0c574fb
inpac1 <ian@miseguroaqui.com>
2024-12-09 20:02:59 -0600

    added html files for cobranza and vigencia and added model class for Poliza

--- src/main/java/com/example/policy_followup/config/SecurityConfig.java
+++ src/main/java/com/example/policy_followup/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.policy_followup;
+package com.example.policy_followup.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
